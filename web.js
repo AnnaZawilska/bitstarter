5f74abc00e0d0d3575a3c70de3689d63c411d3cd
Anna Zawilska <anna.zawilska@gmail.com>
2013-07-08 20:07:45 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var js = require('js');
 var app = express.createServer(express.logger());
-
+var buff = new Buffer(16);
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World2!');
+    buff = fs.readFileSync("index.html");
+    response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
